b7f3e42e240b27eb3a6b726d975674324fda033b
SrivaniVanaparthi2024 <srivanivanaparthi2024@gmail.com>
2026-02-11 18:40:46 +0530

    added thymeleaf demos

---
+++ Sts_Rev_practice/MVC/Spring-mvc-thymleafDemo1/src/main/java/com/example/demo/SpringMvcThymleafDemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcThymleafDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcThymleafDemo1Application.class, args);
+	}
+
+}
